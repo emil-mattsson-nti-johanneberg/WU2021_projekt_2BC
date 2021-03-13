fa6de9899b71e341e569399d914021fbb41eb4a8
emil-mattsson-nti-johanneberg <emil.mattsson@elev.ga.ntig.se>
2021-03-13 19:55:52 +0100

    13/03-2021

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const menuIcon = document.querySelector('.hamburger-menu')
+const navbar = document.querySelector('.nav-lista');
+
+menuIcon.addEventListener('click', () => {navbar.classList.toggle("change");})
