e3cfcfc8dce0502bf4c06076146c0469194e7869
emil-mattsson-nti-johanneberg <emil.mattsson@elev.ga.ntig.se>
2021-03-24 10:04:22 +0100

    23/03-2021
    lyckades lösa hamburger menyn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 const menuIcon = document.querySelector('.hamburger-menu')
-const navbar = document.querySelector('.nav-lista');
+const navbar = document.querySelector('.navbar');
 
 menuIcon.addEventListener('click', () => {navbar.classList.toggle("change");})
